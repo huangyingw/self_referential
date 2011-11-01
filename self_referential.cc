6e41ddf8af8606c778ea51e1acd0e55dac96047d
huangyingw@linux_programming <huangyingw@gmail.com>
2011-11-01 09:49:13 +0800

    n

--- self_referential.cc
+++ self_referential.cc
@@ -4,31 +4,35 @@
 
 using namespace std;
 
-int nUpCount(int a[ ], int len, int n)
+int isSelfReferential(int a[ ], int len)
 {
-  int  partial_sum=0, result=0;
-  for (int i=0;i<len;i++)
-  {
-    if ( partial_sum <= n)
-    {
-      partial_sum+=a[i];
-      if (partial_sum > n)
-        result++;
-    }
-    else
-      partial_sum+=a[i];
-  }
-  return result;
+  bool isSelf=true;
+  if (isSelf)
+    return 1;
+  else
+    return 0;
 }
 int main() 
 {
-  int a[] ={2, 3, 1, 0, -6, 8, -3, -1, 2} ;//3
-  cout<<nUpCount(a,sizeof(a)/sizeof(int),5)<<endl;
+  int a[] ={1, 2, 1, 0} ;//1 7 is the smallest prime in the array and there are 7 primes
+  cout<<isSelfReferential(a,sizeof(a)/sizeof(int))<<endl;
 
-  int b[]={6, 3, 1};//1
-  cout<<nUpCount(b,sizeof(b)/sizeof(int),5)<<endl;
+  int b[]={2, 0, 2, 0};//1 3 is the smallest prime but there are 4 primes in the array
+  cout<<isSelfReferential(b,sizeof(b)/sizeof(int))<<endl;
 
-  int c[]={1, 2, -1, 5, 3, 2, -3};//0
-  cout<<nUpCount(c,sizeof(c)/sizeof(int),20)<<endl;
+  int c[]={2, 1, 2, 0, 0};//0 the array must contain at least one prime
+  cout<<isSelfReferential(c,sizeof(c)/sizeof(int))<<endl;
+
+  int d[]={3, 2, 1, 1, 0, 0, 0};//0 3 is the smallest prime but there is only 1 prime in the array
+  cout<<isSelfReferential(d,sizeof(d)/sizeof(int))<<endl;
+
+  int e[]={4, 2, 1, 0, 1, 0, 0, 0};//1 3 is the smallest prime and there are 3 primes in the array
+  cout<<isSelfReferential(e,sizeof(e)/sizeof(int))<<endl;
+
+  int f[]={5, 2, 1, 0, 0, 1, 0, 0, 0};//0 the array must contain at least one prime.
+  cout<<isSelfReferential(f,sizeof(f)/sizeof(int))<<endl;
+
+  int g[]={6, 2, 1, 0, 0, 0, 1, 0, 0, 0};//0 the array must contain at least one prime.
+  cout<<isSelfReferential(g,sizeof(g)/sizeof(int))<<endl;
   return 0;
 }
